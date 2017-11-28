50d6d6b88546b082c1dccd62f529bc401038eb0c
Paul Soloch <paul.soloch@hotmail.com>
2017-11-28 10:28:21 +0100

    Dépot initial de mon thème

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+    /* Fichier obligatoire pour tout thème wp (l'autre étant style.css) 
+    Il est utiliosé pour afficher une page si aucun autre fichier n'est disponible 
+    https://developer.wordpress.org/themes/basics/template-hierarchy/
+    */
+?>
+
+           
+           
+        <div class="page">
+            <section>
+                <aside>
+                    <img src="./img/illustration.jpg" 
+                        alt="Campus Saint-Marc Devanture">
+                </aside>
+                <article>
+                    <h3>Le Campus Saint-Marc</h3>
+                    <p>« 80 % des métiers d’aujourd’hui auront disparu dans 20 ans » disent les experts.</p>
+                    <p>Les métiers du <span>management</span>, du <span></span>marketing</span>, de la <span>santé</span>, du <span>bien être</span> survivront mais <u>les pratiques vont significativement évoluer...</u> C’est la conviction du Campus Saint Marc. Hyper connexion, dématérialisation, mondialisation : l’environnement dans lequel nous vivons nous amène sans cesse à <span>découvrir de nouveaux outils</span>, à concevoir de nouveaux process.</p>
+                    <p>Dans un tel contexte, <u>il faut former des esprits créatifs, flexibles et entreprenants.</u> Les dernières innovations du campus saint marc <span>vont précisément dans ce sens</span> avec le lancement de l’accélérateur de startups   <span>NFactory</span>.</p>
+                </article>
+
+                <!-- PARTENAIRES -->
+                <div class="partenaires">
+                    <h2>Nos Partenaires</h2>
+                    <ul>
+                        <li><img src="./img/logos/cp11.jpg" alt="Logo 1"></li>
+                        <li><img src="./img/logos/fond_norbert_segard.jpg" alt="Logo 2"></li>
+                        <li><img src="./img/logos/frenchtech.jpg" alt="Logo 3"></li>
+                        <li><img src="./img/logos/logo_cge_web.jpg" alt="Logo 4"></li>
+                    </ul>
+                </div> <!-- /.partenaires -->
+            </section>
+        </div> <!-- /.page -->
+        
